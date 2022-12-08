5f6a0256aecc7b29b221b09f6f91b53ff7175aad
AsresDesta <asres8@gmail.com>
2022-12-08 05:41:20 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
+
 /**
- * main - main block
- * return: 0
+ * main - Entry Point
+ * return: Always 0 (Success)
  */
-int mai(void)
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
+
+
 }
