1c0a1409cb43248be0b367dfa378f37957668d88
AsresDesta <asres8@gmail.com>
2022-12-13 23:47:59 -0800

    Write a program that finds and prints the first 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
-
-
 /**
- * numlength - returns the length of string
- * @num : operand number
- * Return: number of digits
- */
-int numlength(int num)
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (exclude), followed by a new line
+ * Return: 0
+  */
+int main(void)
 {
-	int length = 0;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	if (!num)
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
 	{
-		return (1);
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
+	bef1 = (bef / 1);
+	bef2 = (bef % 1);
+	aft1 = (aft / 1);
+	aft2 = (aft % 1);
 
-	while (num)
+	for (i = 92; i < 99; ++i)
 	{
-		num = num / 10;
-		length += 1;
+		printf(", %lu", aft1 + (aft2 / 1));
+		printf("%lu", aft2 % 1);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
+	printf("\n");
+	return (0);
 
-	return (length);
 }
