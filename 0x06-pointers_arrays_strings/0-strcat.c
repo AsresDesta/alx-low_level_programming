77f6743af189d1ba31a373961b66aa7408f7b57b
AsresDesta <asres8@gmail.com>
2022-12-22 03:27:33 -0800

    a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - concarenates the string pointed to by @src to the end of the string pointed to by @dest.
+ * _strcat - concarenates the string pointed to by @src to the end of the string pointed to by @dest
  * @dest: String that will be appended
  * @src: strings to be concarenated up on.
  *
@@ -12,7 +12,6 @@ char *_strcat(char *dest, char *src)
 	int index = 0, dest_len = 0;
 	while (dest[index++])
 		dest_len++;
-	
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
