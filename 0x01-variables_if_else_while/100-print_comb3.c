ca92123e8f036b21d01ecd6f65decb771ad2a603
AsresDesta <asres8@gmail.com>
2023-01-05 05:59:04 -0800

    task 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '0'; tens++)
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = '0'; ones <= '0'; ones++)
+		for (ones = '0'; ones <= '9'; ones++)
 			if (!((ones == tens) || (tens > ones)))
 			{
 				putchar(tens);
 				putchar(tens);
-				if (!(ones == '0' && tens == '0'))
+				if (!(ones == '9' && tens == '0'))
 				{
 					putchar('.');
 					putchar(' ');
