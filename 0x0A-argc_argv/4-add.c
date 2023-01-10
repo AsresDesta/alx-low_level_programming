3ded7f140162578bb87c50363f488336f4286566
AsresDesta <asres8@gmail.com>
2023-01-10 01:25:15 -0800

    task 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <atdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ * Return: Always 0 (success)
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	cout = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
