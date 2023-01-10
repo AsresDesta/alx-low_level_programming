9f29dcf164af4c2d0645a083cd825f2bfb1f0e4c
AsresDesta <asres8@gmail.com>
2023-01-10 01:44:29 -0800

    task 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,3 +24,35 @@ int check_num(char *str)
 	}
 	return (1);
 }
+/**
+ * main - print the name of the program 
+ * @argc: count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count =1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		printf("%d\n", sum);
+
+		return(0);
+	}
+}
+
