2124fafe49aad4f48633851caed6458acea17b03
AsresDesta <asres8@gmail.com>
2023-01-11 04:20:41 -0800

    task 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	/*Define values with malloc*/
-	buffer = (char *) malloc(size * size * sizeof(c));
+	buffer = (char *) malloc(size * sizeof(c));
 
 	if (buffer == 0)
 	{
