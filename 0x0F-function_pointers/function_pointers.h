3294e2970e957c9bce0ac80b4b6c3a0faa951ec7
AsresDesta <asres8@gmail.com>
2023-01-18 02:31:06 -0800

    Task 0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-Void _putchar(char *c);
+void _putchar(char *c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
