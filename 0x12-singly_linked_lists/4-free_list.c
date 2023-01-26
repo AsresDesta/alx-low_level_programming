0276be33c5323014dc143326b312878715bb542b
AsresDesta <asres8@gmail.com>
2023-01-26 02:58:14 -0800

    task 0-print_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elemnts of a list_t list
+ * @h: The list_t list
+ *
+ * Return: the number of nodes in h
+ */
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elemnts of a list_t list
+ * @h: The list_t list
+ *
+ * Return: the number of nodes in h
+ */
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elemnts of a list_t list
+ * @h: The list_t list
+ *
+ * Return: the number of nodes in h
+ */
+
